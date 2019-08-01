f36c921897f0cbff9c4630bba5d718d5106010de
mjtabaya <mjtabaya@gmail.com>
2019-08-01 20:42:58 +0800

    Barebones devise phase

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'list#index'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    resources :list do
+      resources :task
+    end
+  end
 end
